1a92feadc0d6827a544a09b9ed39b98678c0b7a4
shiohiyoko <takagif315@gmail.com>
2023-06-07 10:39:35 +0200

    WIP

--- config/cmdline.py
+++ config/cmdline.py
@@ -28,17 +28,15 @@ import random
 t = []
 t.append(transforms.Resize(512, interpolation=Image.BICUBIC))
 transform = transforms.Compose(t)
-metadata = MetadataCatalog.get('coco_2017_train_panoptic')
 all_classes = [name.replace('-other','').replace('-merged','') for name in COCO_PANOPTIC_CLASSES] + ["others"]
 colors_list = [(np.array(color['color'])/255).tolist() for color in COCO_CATEGORIES] + [[1, 1, 1]]
 
-def infer_image(model, input_image,computImage=True):
+def infer_image(model, input_image, text_prompt):
     image_ori = transform(input_image)
     width = image_ori.size[0]
     height = image_ori.size[1]
     #print ("image size: ", width, "x",height)
     image_ori = np.asarray(image_ori)
-    visual = Visualizer(image_ori, metadata=metadata)
     images = torch.from_numpy(image_ori.copy()).permute(2,0,1).cuda()
 
     # stroke_inimg = None
@@ -53,13 +51,26 @@ def infer_image(model, input_image,computImage=True):
     model.model.task_switch['audio'] = False
 
     batch_inputs = [data]
-    model.model.metadata = metadata
+    model.model.task_switch['grounding'] = True
+    data['text'] = [text_prompt]
+    results,image_size,extra = model.model.evaluate_demo(batch_inputs)
 
-    results = model.model.evaluate(batch_inputs)
-    pano_seg = results[-1]['panoptic_seg'][0]
-    pano_seg_info = results[-1]['panoptic_seg'][1]
-    tensorNumpy  = pano_seg.cpu().numpy()
-    demo = visual.draw_panoptic_seg(pano_seg.cpu(), pano_seg_info) # rgb Image
-    res = demo.get_image()
-    return Image.fromarray(res),tensorNumpy,pano_seg_info
-    #return tensorNumpy,pano_seg_info
+    pred_masks = results['pred_masks'][0]
+    v_emb = results['pred_captions'][0]
+    t_emb = extra['grounding_class']
+
+    t_emb = t_emb / (t_emb.norm(dim=-1, keepdim=True) + 1e-7)
+    v_emb = v_emb / (v_emb.norm(dim=-1, keepdim=True) + 1e-7)
+
+    temperature = model.model.sem_seg_head.predictor.lang_encoder.logit_scale
+    out_prob = vl_similarity(v_emb, t_emb, temperature=temperature)
+    
+    matched_id = out_prob.max(0)[1]
+    pred_masks_pos = pred_masks[matched_id,:,:]
+    pred_class = results['pred_logits'][0][matched_id].max(dim=-1)[1]
+    
+    pred_masks_pos = (F.interpolate(pred_masks_pos[None,], image_size[-2:], mode='bilinear')[0,:,:data['height'],:data['width']] > 0.0).float().cpu().numpy()
+
+    torch.cuda.empty_cache()
+
+    return pred_masks_pos[0], pred_class
