d548b4f4d77346bc45a9df5f52d97b75bad1f70f
Niklas Bieck <niklas.bieck@etu.univ-st-etienne.fr>
2023-06-07 10:34:01 +0200

    Add option for per-image latents

--- gradio_app.py
+++ gradio_app.py
@@ -6,6 +6,7 @@ import subprocess
 import os
 import shutil
 import sys
+import commentjson
 
 HEADER_TEXT = """
 # Hold My NeRF
@@ -52,6 +53,15 @@ def run_nerf(params):
                         "--aabb_scale", "1",
                         "--video_fps", str(int(100 / video_length)),
                         "--overwrite"], cwd=tempdir)
+
+        if params[use_per_image]:
+            with open(os.path.join(tempdir, "transforms.json"), "r") as transforms:
+                data = commentjson.load(transforms)
+            data["n_extra_learnable_dims"] = 16
+            with open(os.path.join(tempdir, "transforms.json"), "w") as transforms:
+                commentjson.dump(data, transforms)
+            shutil.copy2(os.path.join(tempdir, "transforms.json"), gradio_dir)
+
         print("Training NeRF")
         subprocess.run([sys.executable,
                         os.path.join(ROOT_DIR, "dependencies/instant-ngp/scripts/run.py"),
@@ -92,13 +102,14 @@ if __name__ == "__main__":
                     text_prompt.render()
 
                     with gr.Accordion("NeRF Parameters", open=False):
+                        use_per_image = gr.Checkbox(value=True, label="Per Image Latents")
                         n_steps = gr.Number(value=10000, label="#Steps", precision=0)
 
                     with gr.Row():
                         preview = gr.Button("Preview Segmentation")
                         preview.click(fn=preview_segmentation, inputs={video, text_prompt}, outputs=[segmentation], api_name="preview")
                         run = gr.Button("Submit")
-                        run.click(fn=run_nerf, inputs={video, text_prompt, n_steps}, outputs=[model, checkpoint_file], api_name="nerf")
+                        run.click(fn=run_nerf, inputs={video, text_prompt, n_steps, use_per_image}, outputs=[model, checkpoint_file], api_name="nerf")
 
 
             with gr.Column():
