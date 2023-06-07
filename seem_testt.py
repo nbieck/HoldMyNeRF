d6693fa74d7e025df1654ebc0ccc0a6fa4a7a80b
shiohiyoko <takagif315@gmail.com>
2023-06-07 12:24:51 +0200

    fixed the path to SEEM

--- seem_testt.py
+++ seem_testt.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), "dependencies/SEEM/demo_code"))
+os.chdir(os.path.join(os.getcwd(), 'dependencies/SEEM/demo_code'))
 from seem_extraction import SEEMPipeline
 
 if __name__ == "__main__":
