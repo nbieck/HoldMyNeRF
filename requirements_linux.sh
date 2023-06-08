cadb9984ac63530176de68f701ffbda550c0e977
Niklas Bieck <niklas.bieck@etu.univ-st-etienne.fr>
2023-06-08 12:06:52 +0200

    Add scripts to install pip requirements on linux/windows

---
+++ requirements_linux.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+
+pip3 install -r requirements/linux/requirements.txt
+pip3 install -r requirements/linux/requirements_git.txt
---
+++ requirements_windows.bat
@@ -0,0 +1,2 @@
+pip install -r requirements/windows/torch.txt
+pip install -r requirements/windows/r.txt
